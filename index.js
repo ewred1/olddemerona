ceef29efd0a16164c0af5d5b711ed1b30dc271f8
ewred1 <69081852+ewred1@users.noreply.github.com>
2021-01-03 00:50:26 +0900

    12

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-document.querySelector('._tab_talk>a')
+function regist()
+{
+    console.log(typeof document.querySelector('#name'));
+}
+
+function moveToGoal()
+{
+
+}
+
+
+var hello = "안녕하세요"
+  , hi    = "안녕" 
+  , love  = "사랑해"
+;
+
+function formoon (var k = 1, k<51, k++) {
+    k + (k+2)
+}
+
+// 한줄주석
+/*
+블록주석
+*/
